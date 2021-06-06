6338caa279aaada6606af41bc056dff4ceece17f
Lekhman-vold <volodya.l@yahoo.com>
2021-06-06 22:50:35 +0300

    add static

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 # Default primary key field type
